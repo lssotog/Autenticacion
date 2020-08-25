e31d8bce2507ebea251a7390a97df692ecafbdfb
Luz Stella <lsotog@gmail.com>
2020-08-25 08:08:01 -0500

    Parte final Put

--- app.js
+++ app.js
@@ -28,15 +28,29 @@ server.post ("/usuario", ValidarUsuario, (req, res, next)=>{
     res.json(req.body)
 });
 
-server.put ("/usuarios/:email", (req, res, next)=>{ //cuando se hace el proceos en postman el paramentro :email no se escribe asi sino el correo del usuario
+
+//se vuelve a validar el usuario para verificar que venga la información completa
+server.put ("/usuarios/:email", ValidarUsuario, (req, res, next)=>{ //cuando se hace el proceos en postman el paramentro :email no se escribe asi sino el correo del usuario
     let emailReq = req.params.email;
-    let usuarioEncontrado = arrayUsuario.find((user)=>{
-        return user.email == emailReq;
-    })
-    if (usuarioEncontrado){
-        res.json ({message: "Si encontre el usuario"})
-    } else {
-        res.json ({message: "No encontre el usuario"})
+    if (emailReq !=req.body.email){
+        res.status (409); // status 409 = conflict
+        res.json({message:"El email no coincide"})
+    }else {
+        let usuarioEncontrado = arrayUsuario.find((user)=>{
+            return user.email == emailReq;
+        })
+        if (usuarioEncontrado){
+            let index = arrayUsuario.indexOf (usuarioEncontrado); //se busca en el array la posicion del usuario encontrado para luego actualizarlo
+            //antes que responda se acualiza el usuario
+            arrayUsuario[index].id = req.body.id;
+            arrayUsuario[index].nombre = req.body.nombre;
+            arrayUsuario[index].apellido = req.body.apellido;
+            arrayUsuario[index].contrasena = req.body.contrasena
+           
+            res.json ({message: "Usuario actualizado satisfactoriamente"})
+        } else {
+            res.json ({message: "Usuario no encontrado"})
+        }
     }
 });
 
